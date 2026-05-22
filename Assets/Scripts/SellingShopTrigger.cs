862cecc5957df12a857a273b4cadb0382d549b7b
Leon <2312700@dlu.edu.vn>
2026-05-22 23:23:45 +0700

    Fix va chạm shop bán hàng

--- Assets/Scripts/SellingShopTrigger.cs
+++ Assets/Scripts/SellingShopTrigger.cs
@@ -5,12 +5,16 @@ public class SellingShopTrigger : MonoBehaviour
     // Hàm tự động chạy khi Player bước vào vùng gạch của cửa hàng
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Kiểm tra xem có đúng là Player chạm vào không nhờ vào Tag "Player"
+        Debug.Log("Something entered: " + collision.name);
+
         if (collision.CompareTag("Player"))
         {
-            // Ra lệnh cho ShopUIManager mở màn hình chính lên
+            Debug.Log("PLAYER DETECTED");
+
             if (SellingShopUIManager.Instance != null)
             {
+                Debug.Log("OPEN SHOP");
+
                 SellingShopUIManager.Instance.OpenMainShop();
             }
         }
@@ -19,13 +23,14 @@ public class SellingShopTrigger : MonoBehaviour
     // Hàm tự động chạy khi Player đi ra khỏi vùng gạch của cửa hàng
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            // Ra lệnh ẩn màn hình chính đi
-            if (SellingShopUIManager.Instance != null)
-            {
-                SellingShopUIManager.Instance.CloseMainShop();
-            }
-        }
+        //Debug.Log("EXIT");
+
+        //if (collision.CompareTag("Player"))
+        //{
+        //    if (SellingShopUIManager.Instance != null)
+        //    {
+        //        SellingShopUIManager.Instance.CloseMainShop();
+        //    }
+        //}
     }
 }
